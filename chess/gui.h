6f9fa9d7b4516707391bbaec90b61c1a869d6df4
awlik <smerecukarsen@gmail.com>
2025-03-17 20:23:48 +0000

    ...

--- chess/gui.h
+++ chess/gui.h
@@ -3,4 +3,10 @@
 
 #include <stdint.h>
 
+typedef struct{
+    GLuint textureID;
+    int imgWidth, imgHeight;
+} Texture;
 void draw_chess_board();
+void drawSprite(Texture texture, float x, float y, float spriteWidth, float spriteHeight, int sx, int sy, int sw, int sh);
+void loadTexture(Texture* texture, const char* filename);
--- main.c
+++ main.c
@@ -6,18 +6,24 @@
 #include "chess/gui.h"
 
 GLFWwindow* window;
-
+Texture texture;
 
 __attribute__((constructor)) void init(){
 	init_window(&window, 1024, 1024);
+	loadTexture(&texture, "chess/Chess_Pieces_Sprite.png");
+	glDisable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 int main(void){
 
 	while(!glfwWindowShouldClose(window)){
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 
 		draw_chess_board();
+		drawSprite(texture, -0.5f, -0.5f, 1.0f, 1.0f, 32, 32, 64, 64);
 
 		glfwSwapBuffers(window);
 		
